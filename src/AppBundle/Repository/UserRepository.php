b9aa358885c3c9a587d71fa6a816e294359a8508
german <you@example.com>
2019-08-26 16:35:11 +0300

    User table, user repository are added.

---
+++ src/AppBundle/Repository/UserRepository.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guest
+ * Date: 26.08.19
+ * Time: 16:13
+ */
+
+namespace AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
+
+class UserRepository extends EntityRepository implements UserLoaderInterface
+{
+    //custom query to load user. On default method - e-mail login is allowed only
+    public function loadUserByUsername($username)
+    {
+        return $this->createQueryBuilder('u')
+            ->where('u.username = :username OR u.email = :email')
+            ->setParameter('username', $username)
+            ->setParameter('email', $username)
+            ->getQuery()
+            ->getOneOrNullResult(); //Get exactly one result or null.
+    }
+
+
+}
