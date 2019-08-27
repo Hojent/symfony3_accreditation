939f40a3395d5440b9d999aeb946559906fe7dbf
german <you@example.com>
2019-08-27 17:36:05 +0300

    Region Filter doesn't work :((

---
+++ src/AppBundle/Form/CityRegionFilterType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AppBundle\Form;
+
+use AppBundle\Entity\City;
+use AppBundle\Entity\Region;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CityType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name',TextType::class)
+            ->add('region', EntityType::class, [
+                'class' => Region::class,
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('r')
+                        ->orderBy('r.name', 'ASC');
+                },
+
+                'attr' => ['class' => 'form-control']
+            ])
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => City::class]
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'city';
+    }
+
+
+}
