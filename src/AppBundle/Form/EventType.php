be2b1adedc1e7de55891b3a296b574d892062ff4
german <you@example.com>
2019-09-13 17:16:41 +0300

    Event: + from field TextArea.

--- src/AppBundle/Form/EventType.php
+++ src/AppBundle/Form/EventType.php
@@ -4,6 +4,7 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,7 +17,7 @@ class EventType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('brief')
+            ->add('brief', TextareaType::class, ['attr' => ['class' => 'CKeditor'],])
             ->add('dataStart')
             ->add('dataEnd')
             ->add('dataCreated')
