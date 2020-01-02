4ba899f40a6654fdd366621e308516ff1920bb28
Hojent <irin_german@yahoo.com>
2020-01-02 18:26:29 +0300

    email to admin - page added

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,6 +8,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class DefaultController extends Controller
 {
@@ -15,6 +19,16 @@ class DefaultController extends Controller
      * var $reRegister - if user try to duplicate register
      */
     private $reRegister = 0;
+
+    /**
+     * @var \Swift_Mailer
+     */
+    protected $mailer;
+
+    public function __construct(\Swift_Mailer $mailer)
+    {
+        $this->mailer = $mailer;
+    }
 
     /**
      * @Route("/", name="homepage")
@@ -30,6 +44,48 @@ class DefaultController extends Controller
             'reRegister' => $this->reRegister,
             'events' => $events,
         ]);
+    }
+
+    /**
+     * @Route("/contact", name="contact_email")
+     */
+    public function contactAction(Request $request, \Swift_Mailer $mailer)
+    {
+        $defaultData = ['text' => 'Type your message here'];
+        $form = $this->createFormBuilder($defaultData)
+            ->add('name', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('text', TextareaType::class)
+            ->add('send', SubmitType::class)
+            ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $from = $form->get('email')->getData();
+            $myname = $form->get('name')->getData();
+            $text = $form->get('text')->getData();
+            // data is an array with "name", "email", and "message" keys
+            $message = (new \Swift_Message('EvLogger - Hello Admin'))
+                ->setFrom($from)
+                ->setTo('irin_german@yahoo.com')
+                ->setBody(
+                    $this->renderView(
+                    // app/Resources/views/Emails/registration.html.twig
+                        'default/email.html.twig',
+                        ['name' => $myname, 'from' => $from, 'text' => $text]
+                    ),
+                    'text/html'
+                )
+            ;
+
+            $this->mailer->send($message);
+
+            return $this->render('default/sent.html.twig', ['name' => $myname, 'from' => $from, 'text' => $text] );
+        }
+
+        return $this->render('default/contact.html.twig', [
+            'contact_form' => $form->createView(),
+        ]);
+
     }
 
     /**
